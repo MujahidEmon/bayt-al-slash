701d2b1cd5879fdf3c60c77cc181499793aeba7c
MujahidEmon <mujahidulislam.official@gmail.com>
2025-09-05 21:54:06 +0600

    used menuItem intead of Navlink in Sidebar

--- client/src/components/Dashboard/Sidebar.jsx
+++ client/src/components/Dashboard/Sidebar.jsx
@@ -77,31 +77,11 @@ const Sidebar = () => {
               {/* Statistics */}
               <MenuItem label={'Statistics'} address='/dashboard' icon={BsGraphUp}></MenuItem>
 
-
               {/* Add Room */}
-              <NavLink
-                to='add-room'
-                className={({ isActive }) =>
-                  `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
-                  }`
-                }
-              >
-                <BsFillHouseAddFill className='w-5 h-5' />
+              <MenuItem label={'Add Room'} address='add-room' icon={BsFillHouseAddFill}></MenuItem>
 
-                <span className='mx-4 font-medium'>Add Room</span>
-              </NavLink>
               {/* My Listing */}
-              <NavLink
-                to='my-listings'
-                className={({ isActive }) =>
-                  `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
-                  }`
-                }
-              >
-                <MdHomeWork className='w-5 h-5' />
-
-                <span className='mx-4 font-medium'>My Listings</span>
-              </NavLink>
+              <MenuItem label={'My Listings'} address='my-listings' icon={MdHomeWork}></MenuItem>
             </nav>
           </div>
         </div>
@@ -110,17 +90,8 @@ const Sidebar = () => {
           <hr />
 
           {/* Profile Menu */}
-          <NavLink
-            to='/dashboard/profile'
-            className={({ isActive }) =>
-              `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
-              }`
-            }
-          >
-            <FcSettings className='w-5 h-5' />
+          <MenuItem label={'Profile'} address='/dashboard/profile' icon={FcSettings}></MenuItem>
 
-            <span className='mx-4 font-medium'>Profile</span>
-          </NavLink>
           <button
             onClick={logOut}
             className='flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300   hover:text-gray-700 transition-colors duration-300 transform'
