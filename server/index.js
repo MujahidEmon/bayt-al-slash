4b6c3c9efa487caada267204e976c4437183019e
MujahidEmon <mujahidulislam.official@gmail.com>
2025-09-07 21:03:18 +0600

    Added verifyAdmin func in backend to secure API

--- server/index.js
+++ server/index.js
@@ -52,6 +52,19 @@ async function run() {
     const roomsCollection = client.db("BaytSlashDb").collection("rooms");
     const usersCollection = client.db("BaytSlashDb").collection("users");
 
+    const verifyAdmin = async (req, res, next) => {
+      const user = req.user;
+      const query = { email: user?.email };
+      const result = await usersCollection.findOne(query);
+
+      if (!result || result.role !== "admin")
+        return res.status(401).send({ message: "Forbidden Access" });
+
+      next();
+    };
+
+
+    
     // auth related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
@@ -124,7 +137,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
